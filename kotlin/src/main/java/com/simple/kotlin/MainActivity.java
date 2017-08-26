7f6d23d2ff3bb45b0d55fa0650c693a69050b188
hych <simplehych@gmail.com>
2017-08-26 23:23:12 +0800

    mac test

--- kotlin/src/main/java/com/simple/kotlin/MainActivity.java
+++ kotlin/src/main/java/com/simple/kotlin/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
